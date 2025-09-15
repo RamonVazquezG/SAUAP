19ff3841b1f949d9288aa8c4602451992573cc9b
RamonVazquezG <r1198109@uabc.edu.mx>
2025-09-15 11:10:22 -0700

    capa persistencia-2

--- persistencia/src/main/java/mx/sauap/integration/ServiceLocator.java
+++ persistencia/src/main/java/mx/sauap/integration/ServiceLocator.java
@@ -1,4 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package mx.sauap.integration;
 
+import jakarta.persistence.EntityManager;
+import mx.sauap.dao.*;
+import mx.sauap.persistence.HibernateUtil;
+
+
+/**
+ *
+ * @author total
+ */
 public class ServiceLocator {
+
+    private static ProfesorDAO profesorDAO;
+    private static UsuarioDAO usuarioDAO;
+    private static AsignacionDAO asignacionDAO;
+    private static UnidadAprendizajeDAO unidadAprendizajeDAO;
+
+    private static EntityManager getEntityManager(){
+        return HibernateUtil.getEntityManager();
+    }
+
+    /**
+     * se crea la instancia para alumno DAO si esta no existe
+     */
+    public static ProfesorDAO getInstanceProfesorDAO(){
+        if(profesorDAO == null){
+            profesorDAO = new ProfesorDAO(getEntityManager());
+            return profesorDAO;
+        } else{
+            return profesorDAO;
+        }
+    }
+    /**
+     * se crea la instancia de usuarioDAO si esta no existe
+     */
+    public static UsuarioDAO getInstanceUsuarioDAO(){
+        if(usuarioDAO == null){
+            usuarioDAO = new UsuarioDAO(getEntityManager());
+            return usuarioDAO;
+        } else{
+            return usuarioDAO;
+        }
+    }
+
+    public static AsignacionDAO getInstanceAsignacionDAO(){
+        if(asignacionDAO == null){
+            asignacionDAO = new AsignacionDAO(getEntityManager());
+            return asignacionDAO;
+        } else{
+            return asignacionDAO;
+        }
+    }
+
+    public static UnidadAprendizajeDAO getInstanceUnidadAprendizajeDAO(){
+        if(unidadAprendizajeDAO == null){
+            unidadAprendizajeDAO = new UnidadAprendizajeDAO(getEntityManager());
+            return unidadAprendizajeDAO;
+        } else{
+            return unidadAprendizajeDAO;
+        }
+    }
+
 }
